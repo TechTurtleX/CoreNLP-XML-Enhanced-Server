03da776e8018f447ab1b73c2a6f5f872ccb7485b
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2022-10-11 07:28:39 +0000

    Heroku deploy

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -3,4 +3,4 @@
     Copyright (C) 2013 Niels Lohmann
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms o
+    it under the terms of the GNU General Pu
