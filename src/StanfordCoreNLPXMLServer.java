1e8488b63a88e54a80c8d91405dddeed315ca4cb
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2022-10-13 10:07:28 +0000

    Remove useless comment

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -4,4 +4,4 @@
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Soft
+    the Free Software Foundation, either v
