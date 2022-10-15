1098f3acbfd110d3fe6edde290add71532a2e868
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2022-10-15 17:40:37 +0000

    Up to date!

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -4,4 +4,5 @@
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, o
+    the Free Software Foundation, either version 3 of the License, or
+    (at y
