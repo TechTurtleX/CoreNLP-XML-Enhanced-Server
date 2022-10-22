a523fbc2bf99c3b11f55c54d573660d9602c4cf6
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2022-10-22 10:01:35 +0000

    removed unneeded comments

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -7,4 +7,5 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be u
+    This program is distributed in the hope that it will be useful,
+    but WIT
