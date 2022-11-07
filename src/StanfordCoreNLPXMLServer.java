34bd9512efe8a092ffb386001f0770ede8ed09bc
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2022-11-07 05:25:34 +0000

    dfdf

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -8,4 +8,5 @@
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied w
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ 
