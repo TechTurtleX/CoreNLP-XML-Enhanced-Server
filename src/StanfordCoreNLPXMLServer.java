28655f2e88b3a1654b4a5f2175bebf525567846c
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2022-11-19 09:02:55 +0000

    Обновлена стабильная версия

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -10,4 +10,6 @@
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more detai
+    GNU General Public License for more details.
+
+    You
