6f839cf9a879fe57224491c00f7604fb6d3084e8
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2023-04-11 15:07:23 +0000

    [maven-release-plugin] prepare release 1.5.0

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -35,4 +35,4 @@ import org.simpleframework.http.core.Container;
 import org.simpleframework.http.core.ContainerServer;
 import org.simpleframework.transport.Server;
 import org.simpleframework.transport.connect.Connection;
-import org.simplefra
+import org.simpleframework.tran
