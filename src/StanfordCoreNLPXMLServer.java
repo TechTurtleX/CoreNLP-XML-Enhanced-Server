a294c0f767907ee73d25b44ae9f81af3647bc071
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2023-09-10 06:18:19 +0000

    update about me page

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -55,4 +55,4 @@ public class StanfordCoreNLPXMLServer implements Container {
     public void handle(Request request, Response response) {
         try {
             int request_number = ++total_requests;
-            log.info("Reques
+            log.info("Request " + request_number + 
