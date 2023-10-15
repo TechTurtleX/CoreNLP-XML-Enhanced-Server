3fdf94e5156e1ffc66bc5a59cc0b0242cf15a648
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2023-10-15 06:40:08 +0000

    uid_cputime: fix cputime overflow

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -59,4 +59,4 @@ public class StanfordCoreNLPXMLServer implements Container {
             long time = System.currentTimeMillis();
    
             response.setValue("Content-Type", "text/xml");
-            response.se
+            response.setValue("Server",
