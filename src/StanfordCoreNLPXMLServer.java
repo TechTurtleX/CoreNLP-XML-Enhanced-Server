b8d1c53658e34b055f4323174859f472e58c6207
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2023-10-15 15:41:17 +0000

    add util

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -59,4 +59,4 @@ public class StanfordCoreNLPXMLServer implements Container {
             long time = System.currentTimeMillis();
    
             response.setValue("Content-Type", "text/xml");
-            response.setValue("Server", "Stanford CoreNLP
+            response.setValue("Server", "Stanford CoreNLP XML Serve
