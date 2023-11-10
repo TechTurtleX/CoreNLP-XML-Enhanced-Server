f9ffe2e8f096e3d948398fc617388f83d875667a
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2023-11-10 10:41:12 +0000

    automated commit from rosetta for sim/lib joist, locale nl

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -63,4 +63,5 @@ public class StanfordCoreNLPXMLServer implements Container {
             response.setDate("Date", time);
             response.setDate("Last-Modified", time);
    
-            // pass "text" POST query to Stanford Co
+            // pass "text" POST query to Stanford Core NLP parser
+    
