d1e2219dd6e253c4177e04a29a8c4ce81e59ebb0
TechTurtleX <131901979+TechTurtleX@users.noreply.github.com>
2024-03-13 11:48:49 +0000

    1.8.0

--- src/StanfordCoreNLPXMLServer.java
+++ src/StanfordCoreNLPXMLServer.java
@@ -94,4 +94,4 @@ public class StanfordCoreNLPXMLServer implements Container {
         SocketAddress address = new InetSocketAddress(port);
         connection.connect(address);
 
-        log.info("Initialized serv
+        log.info("Initialized server at port
